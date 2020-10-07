4c21723638d5119a14f9a18c6a04891b803f758f
Joseph Karnafel <josephkarnafel@gmail.com>
2020-10-07 15:00:26 -0700

    add styling and ability to add books Co-authored-by: AJ Markow <alexanderjmarkow@gmail.com>

--- app.rb
+++ app.rb
@@ -1,10 +1,36 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/album')
-require('./lib/song')
+require('./lib/people')
+require('./lib/authors')
+require('./lib/books')
 require('pry')
 also_reload('lib/**/*.rb')
 require('pg')
 require 'dotenv/load'
 
 DB = PG.connect({:dbname => 'library_test', :password => ENV['PG_PASS']})
+
+get('/')do
+  redirect to('/main')
+end
+
+get('/main')do
+  @books = Book.all
+  erb(:main)
+end
+
+get('/new_book') do
+  erb(:new_book)
+end
+
+post('/new_book') do
+  title = params[:book_title]
+  author = params[:book_author]
+  book = Book.new({ title: title, author: author, id: nil })
+  book.save
+  redirect to('/main')
+end
+
+get('people') do
+  'what it does'
+end
